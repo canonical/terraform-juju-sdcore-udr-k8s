5c48ec324dbba11678cba3c758c834d238635f9f
gatici <gulsum.atici@canonical.com>
2024-01-23 16:16:48 +0300

    Initial Commit
    Signed-off-by: gatici <gulsum.atici@canonical.com>

---
+++ main.tf
@@ -0,0 +1,73 @@
+resource "juju_application" "udr" {
+  name = "udr"
+  model = var.model_name
+
+  charm {
+    name = "sdcore-udr-k8s"
+    channel = var.channel
+  }
+
+  units = 1
+  trust = true
+}
+
+module "mongodb-k8s" {
+  source     = "gatici/mongodb-k8s/juju"
+  model_name = var.model_name
+}
+
+module "self-signed-certificates" {
+  source     = "gatici/self-signed-certificates/juju"
+  model_name = var.model_name
+}
+
+module "sdcore-nrf-k8s" {
+  source  = "gatici/sdcore-nrf-k8s/juju"
+  model_name = var.model_name
+  certs_application_name = var.certs_application_name
+  db_application_name = var.db_application_name
+  channel = var.channel
+}
+
+resource "juju_integration" "udr-db" {
+  model = var.model_name
+
+  application {
+    name     = juju_application.udr.name
+    endpoint = "database"
+  }
+
+  application {
+    name     = var.db_application_name
+    endpoint = "database"
+  }
+}
+
+resource "juju_integration" "udr-certs" {
+  model = var.model_name
+
+  application {
+    name     = juju_application.udr.name
+    endpoint = "certificates"
+  }
+
+  application {
+    name     = var.certs_application_name
+    endpoint = "certificates"
+  }
+}
+
+resource "juju_integration" "udr-nrf" {
+  model = var.model_name
+
+  application {
+    name     = juju_application.udr.name
+    endpoint = "fiveg-nrf"
+  }
+
+  application {
+    name     = var.nrf_application_name
+    endpoint = "fiveg-nrf"
+  }
+}
+
